334b22fab59a113fa69f18ab3a3a3f39ce6d04d9
Logan Curtis <lcurtis0104@gmail.com>
2021-08-26 13:21:06 -0500

    Phase 1 & 2 complete

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int secretNum = 7; //new Random().Next(1,100);
+
+            Console.WriteLine("Guess the Secret Number");
+            Console.WriteLine("Enter a number between 1-100");
+
+            int guess = int.Parse(Console.ReadLine());
+
+            if (guess == secretNum )
+            {
+                Console.WriteLine("Nice Guess! Can you get it twice though??");
+            } 
+            else {
+                Console.WriteLine("Lol, not even close! Try again?");
+            }
+
+
+        }
+    }
+}
