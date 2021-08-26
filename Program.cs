4f1a86271f7934ffc1405ce002440118d09d20ff
Logan Curtis <lcurtis0104@gmail.com>
2021-08-26 13:43:53 -0500

    Phase 3 complete

--- Program.cs
+++ Program.cs
@@ -8,19 +8,29 @@ namespace GuessingGame
         {
             int secretNum = 7; //new Random().Next(1,100);
 
+            int GuessCount = 0;
+
             Console.WriteLine("Guess the Secret Number");
             Console.WriteLine("Enter a number between 1-100");
 
-            int guess = int.Parse(Console.ReadLine());
-
-            if (guess == secretNum )
+            while (GuessCount < 4)
             {
-                Console.WriteLine("Nice Guess! Can you get it twice though??");
-            } 
-            else {
-                Console.WriteLine("Lol, not even close! Try again?");
-            }
+                int guess = int.Parse(Console.ReadLine());
 
+                if (guess == secretNum )
+                {
+                    Console.WriteLine("Nice Guess! Can you get it twice though??");
+                } 
+                else if (guess < secretNum) {
+                    Console.WriteLine("Aim higher");
+                    GuessCount++;
+                }
+                else if (guess > secretNum) {
+                    Console.WriteLine("Chill out, it aint that big");
+                    GuessCount++;
+                }
+                
+            }
 
         }
     }
