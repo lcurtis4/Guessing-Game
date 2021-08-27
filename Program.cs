3c16e63c93012403fe0c7823f9d3bb22150a6536
Logan Curtis <lcurtis0104@gmail.com>
2021-08-27 13:23:06 -0500

    Phase 7

--- Program.cs
+++ Program.cs
@@ -9,44 +9,71 @@ namespace GuessingGame
             int secretNum = new Random().Next(1,100);
 
             int GuessCount = 3;
+            int Difficulty = 0;
             
+            Console.WriteLine("Enter Difficutly");
+            while (Difficulty == 0)
+            {
+                switch(int.Parse(Console.ReadLine()))
+                    {
+                        case 1: 
+                            Difficulty = 1;
+                            GuessCount = 8;
+                            Console.WriteLine("You've Selected Easy. You have 8 guesses");
+                            break;
+                        case 2: 
+                            Difficulty = 2;
+                            GuessCount = 6;
+                            Console.WriteLine("You've Selected Medium. You have 6 guesses");
+                            break;
+                        case 3: 
+                            Difficulty = 3;
+                            GuessCount = 8;
+                            Console.WriteLine("You've Selected Hard. You have 4 guesses");
+                            break;
+                        default: 
+                            Console.WriteLine("Please select dificulty 1-3");
+                            break;
+                    }
+            }
+
             Console.WriteLine("Guess the Secret Number");
             Console.WriteLine("Enter a number between 1-100 you have 4 guesses");
             
             int guess = int.Parse(Console.ReadLine());
-            
-            while (GuessCount < 4)
+
+            while (GuessCount > 1)
             {
                 if (guess == secretNum) 
-                {
-                    Console.WriteLine("You Got it");
-                    break;
-                }
+                    {
+                        Console.WriteLine("You Got it");
+                        break;
+                    }
                 else
-                {
-                    string HighLowString = "too high";
-                    string GuessString = "guesses";
-                    if ((GuessCount) == 2)
                     {
-                        GuessString = "guess";
+                        string HighLowString = "too high";
+                        string GuessString = "guesses";
+                        if ((GuessCount) == 1)
+                        {
+                            GuessString = "guess";
+                        }
+                        if (guess < secretNum)
+                        {
+                            HighLowString = "too low";
+                        }
+                        Console.WriteLine($"Sorry {guess} is {HighLowString}. You have {GuessCount} {GuessString} left, Please Try again");
+                        guess = int.Parse(Console.ReadLine());
+                        GuessCount--;
                     }
-                    if (guess < secretNum)
-                    {
-                        HighLowString = "too low";
-                    }
-                    Console.WriteLine($"Sorry {guess} is {HighLowString}. You have {GuessCount} {GuessString} left, Please Try again");
-                    guess = int.Parse(Console.ReadLine());
-                    GuessCount--;
-                }
                 if (GuessCount == 0)
                     {
                         break;
                     }
             }
             if (guess != secretNum)
-            {
-                Console.WriteLine("Sorry, you're out of guesses");
+                {
+                    Console.WriteLine("Sorry, you're out of guesses");
+                }
             }
         }
     }
-}
